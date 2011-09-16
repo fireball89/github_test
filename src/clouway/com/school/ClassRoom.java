b5c900ee8632b86f827c61642e376903dabe8147
Adelin Ghanaem <adio@evo.bg>
2011-09-16 18:09:42 +0300

    The ClassRoom class is modified

--- src/clouway/com/school/ClassRoom.java
+++ src/clouway/com/school/ClassRoom.java
@@ -5,18 +5,56 @@ package clouway.com.school;
  */
 public class ClassRoom {
 
-	private String className;
-	private Student[] students;
-	private static final int MAX_CLASS_STUDENTS=20;
+    private String className;
+    private Student[] students;
+    private static final int MAX_CLASS_STUDENTS = 20;
 
     /**
      * Creates new ClassRoom object with a specified name
+     *
      * @param className: the name of the class room
      */
-	public ClassRoom(String className){
-		this.className = className;
-		this.students = new Student[MAX_CLASS_STUDENTS]; 
-	} 
+    public ClassRoom(String className) {
+        this.className = className;
+        this.students = new Student[MAX_CLASS_STUDENTS];
+    }
 
+    public String getClassName() {
+        return className;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public static int getMaxClassStudents() {
+        return MAX_CLASS_STUDENTS;
+    }
+
+    /**
+     *
+     * @param student
+     * @param index
+     */
+    public void insertStudent(Student student, int index) {
+        if (index < 0 && index > MAX_CLASS_STUDENTS - 1) {
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            this.students[index] = student;
+        }
+    }
+
+    /**
+     *
+     * @param index
+     * @return
+     */
+    public Student getStudent(int index) {
+        if (index < 0 && index > MAX_CLASS_STUDENTS - 1) {
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            return this.students[index];
+        }
+    }
 }
 
